99d9e0ae9f7d5390b0061a516eddcd9ced0576bc
soumyathamke <52737099+soumyathamke@users.noreply.github.com>
2023-04-23 19:03:41 -0700

    updating.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from  PIL import Image, ImageEnhance
 
 
 
-image = Image.open(r'...\Insights_Bees_logo.png') #Brand logo image (optional)
+ #Brand logo image (optional)
 
 #Create two columns with different width
 col1, col2 = st.columns( [0.8, 0.2])
