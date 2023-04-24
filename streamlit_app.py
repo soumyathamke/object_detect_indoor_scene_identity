57d007b3113cfb9258344518303ab63bcd6d5ce8
soumyathamke <52737099+soumyathamke@users.noreply.github.com>
2023-04-23 18:59:26 -0700

    updating.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 
-import streamlit as st
-st.write(""" my first app""")
+image = Image.open(r'...\Insights_Bees_logo.png') #Brand logo image (optional)
+
+#Create two columns with different width
+col1, col2 = st.columns( [0.8, 0.2])
+with col1:               # To display the header text using css style
+    st.markdown(""" <style> .font {
+    font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+    </style> """, unsafe_allow_html=True)
+    st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
+    
+with col2:               # To display brand logo
+    st.image(image,  width=150)
